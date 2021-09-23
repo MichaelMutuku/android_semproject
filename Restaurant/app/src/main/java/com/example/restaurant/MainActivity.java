61088c82d44350ecf1d142c93cb0e177fb2a4b7c
Michael Mutuku <themikelyway.gmail.com>
2021-09-23 12:34:00 +0300

    created initial project tempelate

---
+++ Restaurant/app/src/main/java/com/example/restaurant/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.restaurant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void launchApp(View view) {
+        Intent intent = new Intent(this, Things.class);
+        startActivity(intent);
+    }
+}
